73b35baf5f0ae931d1ce5ae49b4b28e9922bff82
Victor <victorcmuniz@gmail.com>
2022-04-23 10:28:47 -0300

    2.1 - Desenho do X e O

--- script.js
+++ script.js
@@ -22,4 +22,39 @@ function paintBoard() {
   ctx.stroke();
 }
 
+function drawX(x, y) {
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+
+  x = x * blockSize;
+  y = y * blockSize;
+
+  offset = 10;
+
+  ctx.moveTo(x + offset, y + offset);
+  ctx.lineTo(x + blockSize - offset, y + blockSize - offset);
+
+  ctx.moveTo(x + offset, y + blockSize - offset);
+  ctx.lineTo(x + blockSize - offset, y + offset);
+
+  ctx.stroke();
+}
+
+drawX(2, 2);
+
+function drawO(x, y) {
+  ctx.beginPath();
+
+  x = x * blockSize + blockSize / 2;
+  y = y * blockSize + blockSize / 2;
+  offset = 10;
+  radius = blockSize / 2 - offset;
+
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+
+  ctx.stroke();
+}
+
+drawO(2, 1);
+
 paintBoard();
